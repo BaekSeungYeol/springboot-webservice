209a4d912a05689b0c8d02f95da83a0b958d8bca
BaekSeungYeol <beck33333@naver.com>
2020-01-14 13:59:10 +0900

    Test 리팩토링

--- src/main/java/com/whitewin/web/IndexController.java
+++ src/main/java/com/whitewin/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.whitewin.web;
 
+import com.whitewin.config.auth.LoginUser;
 import com.whitewin.config.auth.dto.SessionUser;
 import com.whitewin.domain.user.User;
 import com.whitewin.service.posts.PostsService;
@@ -17,14 +18,10 @@ import javax.servlet.http.HttpSession;
 public class IndexController {
 
     private final PostsService postsService;
-    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
-
-        SessionUser user = (SessionUser) httpSession.
-                getAttribute("user");
 
         if(user != null) {
             model.addAttribute("userName", user.getName());
